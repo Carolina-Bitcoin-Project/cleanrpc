cbda5d3d5a4eac88067cc297f46fc1031f33b420
kg <keithgardner@icloud.com>
2023-11-06 09:35:43 -0500

    gem cleanrpc

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in cleanrpc.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/cleanrpc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cleanrpc/version"
+
+module Cleanrpc
+  class Error < StandardError; end
+  # Your code goes here...
+end
